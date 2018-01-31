7eb1da8a0ab37cdc9b3f534da8d7d109f4aead13
Christopher Madan <cMadan@users.noreply.github.com>
2018-01-31 16:42:23 +0000

    developing depth

--- calcSulc/calcSulc_depth.m
+++ calcSulc/calcSulc_depth.m
@@ -1,1 +1,9 @@
+function width = calcSulc_depth(options,subject_hemi,mesh,sulcmap,gyrifmesh)
+
 %% fundus (valley) (for depth)
+% find vertices for lowest points in map, using sulcal map
+% lowest 100 vertices
+
+keyboard
+
+% calculate distance between vertex on pial surface and gyrif surface vertex
