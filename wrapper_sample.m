1e3ec56cc152fdba80bfba03fabd7a610c71177f
Christopher Madan <Christopher.Madan@nottingham.ac.uk>
2018-02-05 18:48:08 +0000

    notes for FS53 compatibility

--- wrapper_sample.m
+++ wrapper_sample.m
@@ -4,10 +4,18 @@ subject_dir = 'D:\Research\calcSulc\data';
 subjects = {'.'};
 
 % config settings that we would expect to be passed into the wrapper
+% FS60 labels
 options.list_sulc = {'S_central','S_front_sup','S_front_inf',...
                      'S_postcentral','S_oc_middle&Lunatus',...
                      'S_cingul-Marginalis','S_parieto_occipital','S_oc-temp_med&Lingual'};
-                 
+          
+%{
+% need slightly different aparc names for FS53 (than FS60)
+options.list_sulc = {'S_central','S_front_sup','S_front_inf',...
+                     'S_postcentral','S_oc_middle_and_Lunatus',...
+                     'S_cingul-Marginalis','S_parieto_occipital','S_oc-temp_med_and_Lingual'};                 
+%}
+		  
 % settings
 options.setWidthWalk  = 4;
 options.estimateWidth = 1;
